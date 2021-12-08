e57bb439085e21283ef2d5902419dd85bee3c346
Jakirul <jakirul.islam@live.co.uk>
2021-12-08 10:58:36 +0000

    added id and post route

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -13,4 +13,25 @@ router.get('/', async(req, res) => {
     }
 }) 
 
+router.get('/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const post = await Post.findById(id);
+        res.status(200).send(post)
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+router.post('/', async(req, res) => {
+    try {
+        const post = await Post.create(req.body);
+        res.status(200).send(post)
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
 module.exports = router;
