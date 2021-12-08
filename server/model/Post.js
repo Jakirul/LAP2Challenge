bf9ca219cb75aa84803fc6c8716ab8ebca19671b
Jakirul <jakirul.islam@live.co.uk>
2021-12-08 10:35:16 +0000

    fixed small typo in post class

--- server/model/Post.js
+++ server/model/Post.js
@@ -24,7 +24,7 @@ class Post {
     static findById(id){
         return new Promise (async (resolve, reject) => {
             try {
-                let postData = await db.query('SELECT * FROM posts WHERE post_id = $1;', [ id ]);
+                let postData = await db.query('SELECT * FROM posts WHERE id = $1;', [ id ]);
                 let post = new Post(postData.rows[0]);
                 resolve(post);
             } catch (err) {
